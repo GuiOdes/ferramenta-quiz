86ca27d0d73fb1dbc635d74a38069a1a20c35145
Guilherme <guilherme.montnezes@gmail.com>
2024-10-29 22:39:50 -0300

    First commit

---
+++ src/main/kotlin/com/sistemas/ferramenta_quiz/FerramentaQuizApplication.kt
@@ -0,0 +1,11 @@
+package com.sistemas.ferramenta_quiz
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FerramentaQuizApplication
+
+fun main(args: Array<String>) {
+	runApplication<FerramentaQuizApplication>(*args)
+}
