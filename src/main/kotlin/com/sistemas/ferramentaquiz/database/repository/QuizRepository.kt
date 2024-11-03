8190bcc174bae2f824b2541386a3a8e27fb7a298
alexandrendev <alxfn86@gmail.com>
2024-11-03 17:20:35 -0300

    fix duplicated QuizDto and rename migration name to V003

--- src/main/kotlin/com/sistemas/ferramentaquiz/database/repository/QuizRepository.kt
+++ src/main/kotlin/com/sistemas/ferramentaquiz/database/repository/QuizRepository.kt
@@ -1,14 +1,14 @@
 package com.sistemas.ferramentaquiz.database.repository
 
 import com.sistemas.ferramentaquiz.database.repository.data.QuizSpringDataRepository
-import com.sistemas.ferramentaquiz.dto.CreateQuizDto
+import com.sistemas.ferramentaquiz.dto.QuizDto
 import org.springframework.stereotype.Repository
 
 @Repository
 class QuizRepository(
     private val repository: QuizSpringDataRepository
 ) {
-    fun save(quiz: CreateQuizDto) = repository.save(
+    fun save(quiz: QuizDto) = repository.save(
         quiz.toEntity()
     )
 }
--- src/main/kotlin/com/sistemas/ferramentaquiz/service/QuizService.kt
+++ src/main/kotlin/com/sistemas/ferramentaquiz/service/QuizService.kt
@@ -3,11 +3,10 @@ package com.sistemas.ferramentaquiz.service
 import com.sistemas.ferramentaquiz.api.request.CreateQuizRequest
 import com.sistemas.ferramentaquiz.database.repository.QuizRepository
 import com.sistemas.ferramentaquiz.database.repository.UserRepository
-import com.sistemas.ferramentaquiz.dto.CreateQuizDto
+import com.sistemas.ferramentaquiz.dto.QuizDto
 import com.sistemas.ferramentaquiz.exception.UserNotFoundException
 import org.apache.commons.lang3.RandomStringUtils
 import org.springframework.stereotype.Service
-import kotlin.random.Random
 
 @Service
 class QuizService(
@@ -18,10 +17,11 @@ class QuizService(
         val user = userRepository.findByEmail(userEmail) ?: throw UserNotFoundException()
         val code = generateAcessCode()
 
-        val dto = CreateQuizDto(request.title,
-            user.toEntity(),
-            code,
-            false)
+        val dto = QuizDto(
+            title = request.title,
+            user = user.toEntity(),
+            code = code,
+            isDone = false)
 
         repository.save(dto)
     }
